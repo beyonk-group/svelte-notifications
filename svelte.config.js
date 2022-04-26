37a8fe34df497ef01beeec02e8cbbfc1694c70ed
Antony Jones <antony@beyonk.com>
2022-04-26 16:47:38 +0100

    upgrade SvelteKit

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,11 @@
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte'
+    package: {
+      exports: filepath => {
+        return filepath === 'index.js' || filepath.endsWith('.json')
+      }
+    }
   }
 }
 
