13f5850ade57dd10ee4c54ee5df75a39583ce2de
Konstantin Nosov <nosov@nodeart.io>
2023-08-12 14:09:02 +0200

    use latest svelte-kit packaging setup add svelte >=3 for peer deps

--- src/lib/index.d.ts
+++ src/lib/index.d.ts
@@ -1,31 +1,26 @@
 declare module '@beyonk/svelte-notifications' {
-  import { SvelteComponentTyped } from 'svelte/types/runtime'
+	import { SvelteComponentTyped } from 'svelte/types/runtime';
 
-  export type TNotificationTypes =
-    | 'default'
-    | 'danger'
-    | 'warning'
-    | 'info'
-    | 'success'
+	export type TNotificationTypes = 'default' | 'danger' | 'warning' | 'info' | 'success';
 
-  export interface TNotifier {
-    send(message: string, type: TNotificationTypes, timeout?: number): void
-    danger(message: string, timeout?: number): void
-    warning(message: string, timeout?: number): void
-    info(message: string, timeout?: number): void
-    success(message: string, timeout?: number): void
-  }
+	export interface TNotifier {
+		send(message: string, type: TNotificationTypes, timeout?: number): void;
+		danger(message: string, timeout?: number): void;
+		warning(message: string, timeout?: number): void;
+		info(message: string, timeout?: number): void;
+		success(message: string, timeout?: number): void;
+	}
 
-  export interface ITheme {
-    danger: '#bb2124'
-    success: '#22bb33'
-    warning: '#f0ad4e'
-    info: '#5bc0de'
-    default: '#aaaaaa'
-  }
-  type IProps = { themes?: ITheme; timeout?: number; persist?: boolean }
+	export interface ITheme {
+		danger: '#bb2124';
+		success: '#22bb33';
+		warning: '#f0ad4e';
+		info: '#5bc0de';
+		default: '#aaaaaa';
+	}
+	type IProps = { themes?: ITheme; timeout?: number; persist?: boolean };
 
-  export class NotificationDisplay extends SvelteComponentTyped<IProps> {}
+	export class NotificationDisplay extends SvelteComponentTyped<IProps> {}
 
-  export const notifier: TNotifier
+	export const notifier: TNotifier;
 }
--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,13 @@
+import adapter from '@sveltejs/adapter-auto';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    package: {
-      exports: filepath => {
-        return filepath === 'index.js' || filepath.endsWith('.json')
-      }
-    }
-  }
-}
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
+	}
+};
 
-export default config
+export default config;
